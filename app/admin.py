dd817ff921bc4747c23ef16b508f224dcaedf32e
Anton Lööf <anton@loofarna.se>
2021-12-20 17:37:06 +0100

    Only show valid invoices when adding new item
    this means that you have to create a new invoice first
    when adding new fancy items
    
    also removed som useless scripts now that the admin page is up and runngin

--- app/admin.py
+++ app/admin.py
@@ -2,7 +2,11 @@ from django.contrib import admin
 from app.models import ToMemberInvoice, Person, FarnellItem
 
 class FarnellItemAdmin(admin.ModelAdmin):
-    pass
+    def formfield_for_foreignkey(self, db_field, request, **kwargs):
+        if db_field.name == "to_member_invoice":
+            kwargs["queryset"] = ToMemberInvoice.objects.filter(sent=False, payed=False)
+        return super(FarnellItemAdmin, self).formfield_for_foreignkey(db_field, request, **kwargs)
+
     
 class PersonAdmin(admin.ModelAdmin):
     search_fields = ["name"]
@@ -11,6 +15,5 @@ class ToMemberInvoiceAdmin(admin.ModelAdmin):
     search_fields = ["id"]
     
 admin.site.register(ToMemberInvoice, ToMemberInvoiceAdmin)
-    
 admin.site.register(Person, PersonAdmin)
 admin.site.register(FarnellItem, FarnellItemAdmin)
